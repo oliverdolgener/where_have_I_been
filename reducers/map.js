59effdeb7ed72a585bbc71064f1c5bd49ba0e6b1
s48024 <s48024@beuth-hochschule.de>
2018-07-25 22:57:58 +0200

    save mapView

--- reducers/map.js
+++ reducers/map.js
@@ -1,3 +1,4 @@
+import { AsyncStorage } from 'react-native';
 import { Map } from 'immutable';
 
 export const types = {
@@ -12,9 +13,14 @@ const initialState = Map({
   mapView: 'satellite',
 });
 
+const setMapViewAsync = async (mapView) => {
+  await AsyncStorage.setItem('mapView', mapView);
+};
+
 export default (state = initialState, action) => {
   switch (action.type) {
     case types.SETMAPVIEW:
+      setMapViewAsync(action.mapView);
       return state.set('mapView', action.mapView);
     default:
       return state;
--- screens/SplashScreen.js
+++ screens/SplashScreen.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { AsyncStorage, View, StyleSheet, Text } from 'react-native';
+import { connect } from 'react-redux';
+import { actions as mapActions } from '../reducers/map';
 import * as Colors from '../constants/Colors';
 
 const styles = StyleSheet.create({
@@ -19,13 +21,14 @@ const styles = StyleSheet.create({
   },
 });
 
-class LoginScreen extends React.Component {
+class SplashScreen extends React.Component {
   componentWillMount() {
     this.getUserAsync();
   }
 
   getUserAsync = async () => {
     const id = await AsyncStorage.getItem('id');
+    const mapView = await AsyncStorage.getItem('mapView');
     setTimeout(() => {
       if (id) {
         fetch(`https://api.0llum.de/users/${id}`)
@@ -35,6 +38,10 @@ class LoginScreen extends React.Component {
           });
       } else {
         this.props.navigation.navigate('Login');
+      }
+
+      if (mapView) {
+        this.props.setMapView(mapView);
       }
     }, 3000);
   };
@@ -64,4 +71,15 @@ class LoginScreen extends React.Component {
   }
 }
 
-export default LoginScreen;
+const mapStateToProps = state => ({
+  mapView: state.map.get('mapView'),
+});
+
+const mapDispatchToProps = {
+  setMapView: mapActions.setMapView,
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps,
+)(SplashScreen);
