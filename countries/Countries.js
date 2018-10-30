f928acc1ccc44931803e9687d1bdbad5c3dd0ad3
0llum <o.dolgener@googlemail.com>
2018-10-31 00:31:45 +0100

    feat(Countries): add more countries

--- countries/Countries.js
+++ countries/Countries.js
@@ -1,73 +1,151 @@
+import Afghanistan from './Afghanistan';
+import Albania from './Albania';
+import Algeria from './Algeria';
+import Andorra from './Andorra';
+import Angola from './Angola';
 import Argentina from './Argentina';
+import Armenia from './Armenia';
 import Australia from './Australia';
+import Austria from './Austria';
+import Azerbaijan from './Azerbaijan';
+import Belarus from './Belarus';
+import Belgium from './Belgium';
+import Bhutan from './Bhutan';
+import Bolivia from './Bolivia';
 import Brazil from './Brazil';
 import Burma from './Burma';
 import Canada from './Canada';
 import Chile from './Chile';
 import China from './China';
 import Colombia from './Colombia';
+import CostaRica from './CostaRica';
+import Croatia from './Croatia';
+import Cyprus from './Cyprus';
+import CzechRepublic from './CzechRepublic';
 import DemocraticRepublicOfTheCongo from './DemocraticRepublicOfTheCongo';
+import Denmark from './Denmark';
 import DominicanRepublic from './DominicanRepublic';
 import Ecuador from './Ecuador';
 import Egypt from './Egypt';
+import Ethiopia from './Ethiopia';
+import FederatedStatesOfMicronesia from './FederatedStatesOfMicronesia';
+import Finland from './Finland';
 import France from './France';
 import Germany from './Germany';
 import Greece from './Greece';
 import Greenland from './Greenland';
+import Iceland from './Iceland';
 import India from './India';
 import Indonesia from './Indonesia';
+import Ireland from './Ireland';
 import Italy from './Italy';
 import Japan from './Japan';
 import Kazakhstan from './Kazakhstan';
+import Kenya from './Kenya';
+import Lesotho from './Lesotho';
+import Luxembourg from './Luxembourg';
 import Mexico from './Mexico';
 import Mongolia from './Mongolia';
+import Nepal from './Nepal';
+import Netherlands from './Netherlands';
 import NewZealand from './NewZealand';
 import Norway from './Norway';
 import PapuaNewGuinea from './PapuaNewGuinea';
 import Peru from './Peru';
 import Philippines from './Philippines';
+import Portugal from './Portugal';
 import Russia from './Russia';
+import Spain from './Spain';
+import Sweden from './Sweden';
 import Switzerland from './Switzerland';
+import Tanzania from './Tanzania';
 import Thailand from './Thailand';
+import Turkey from './Turkey';
 import Ukraine from './Ukraine';
 import UnitedKingdom from './UnitedKingdom';
 import UnitedStates from './UnitedStates';
+import Uruguay from './Uruguay';
+import Uzbekistan from './Uzbekistan';
+import Vanuatu from './Vanuatu';
+import Venezuela from './Venezuela';
 import Vietnam from './Vietnam';
+import Yemen from './Yemen';
+import Zambia from './Zambia';
+import Zimbabwe from './Zimbabwe';
 
 export default [
+  Afghanistan,
+  Albania,
+  Algeria,
+  Andorra,
+  Angola,
   Argentina,
+  Armenia,
   Australia,
+  Austria,
+  Azerbaijan,
+  Belarus,
+  Belgium,
+  Bhutan,
+  Bolivia,
   Brazil,
   Burma,
   Canada,
   Chile,
   China,
   Colombia,
+  CostaRica,
+  Croatia,
+  Cyprus,
+  CzechRepublic,
   DemocraticRepublicOfTheCongo,
+  Denmark,
   DominicanRepublic,
   Ecuador,
   Egypt,
+  Ethiopia,
+  FederatedStatesOfMicronesia,
+  Finland,
   France,
   Germany,
   Greece,
   Greenland,
+  Iceland,
   India,
   Indonesia,
+  Ireland,
   Italy,
   Japan,
   Kazakhstan,
+  Kenya,
+  Lesotho,
+  Luxembourg,
   Mexico,
   Mongolia,
+  Nepal,
+  Netherlands,
   NewZealand,
   Norway,
   PapuaNewGuinea,
   Peru,
   Philippines,
+  Portugal,
   Russia,
+  Spain,
+  Sweden,
   Switzerland,
+  Tanzania,
   Thailand,
+  Turkey,
   Ukraine,
   UnitedKingdom,
   UnitedStates,
+  Uruguay,
+  Uzbekistan,
+  Vanuatu,
+  Venezuela,
   Vietnam,
+  Yemen,
+  Zambia,
+  Zimbabwe,
 ];
