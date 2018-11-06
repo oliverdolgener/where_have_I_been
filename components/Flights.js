767e157a92d6761ff5d9862c6456a9c25e8ab506
0llum <o.dolgener@googlemail.com>
2018-11-06 23:39:03 +0100

    feat(Flights): show marker at airports

--- components/Flights.js
+++ components/Flights.js
@@ -1,23 +1,38 @@
 import React from 'react';
+import { View, Image } from 'react-native';
 import { MapView } from 'expo';
 import { connect } from 'react-redux';
 
 import * as Colors from '../constants/Colors';
+import iconMarker from '../assets/iconMarker.png';
 
 const Flights = (props) => {
   const { flights, friendFlights } = props;
   const visibleFlights = friendFlights || flights;
   return visibleFlights.map(x => (
-    <MapView.Polyline
-      key={x.id.toString()}
-      coordinates={[
-        { latitude: x.start_latitude, longitude: x.start_longitude },
-        { latitude: x.destination_latitude, longitude: x.destination_longitude },
-      ]}
-      strokeColor={Colors.blue}
-      strokeWidth={1}
-      geodesic
-    />
+    <View key={x.id.toString()}>
+      <MapView.Polyline
+        coordinates={[
+          { latitude: x.start_latitude, longitude: x.start_longitude },
+          { latitude: x.destination_latitude, longitude: x.destination_longitude },
+        ]}
+        strokeColor={Colors.blue}
+        strokeWidth={1}
+        geodesic
+      />
+      <MapView.Marker
+        coordinate={{ latitude: x.start_latitude, longitude: x.start_longitude }}
+        title={x.start_code}
+      >
+        <Image source={iconMarker} style={{ width: 40, height: 40 }} />
+      </MapView.Marker>
+      <MapView.Marker
+        coordinate={{ latitude: x.destination_latitude, longitude: x.destination_longitude }}
+        title={x.destination_code}
+      >
+        <Image source={iconMarker} style={{ width: 40, height: 40 }} />
+      </MapView.Marker>
+    </View>
   ));
 };
 
