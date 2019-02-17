d74171cd072d3f63d5386e0a5f949d4bf6a4050e
0llum <o.dolgener@googlemail.com>
2019-02-17 13:33:12 +0100

    fix(build.gradle): remove debug keystore path

--- android/app/src/main/java/host/exp/exponent/generated/DetachBuildConstants.java
+++ android/app/src/main/java/host/exp/exponent/generated/DetachBuildConstants.java
@@ -3,6 +3,6 @@ package host.exp.exponent.generated;
 // This file is auto-generated. Please don't rename!
 public class DetachBuildConstants {
 
-  public static final String DEVELOPMENT_URL = "whib://192.168.1.177:19000";
+  public static final String DEVELOPMENT_URL = "whib://192.168.178.22:80";
 
 }
